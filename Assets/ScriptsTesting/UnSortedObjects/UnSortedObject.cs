b1a8048132bc404c25bab23698bc383e4e9abfdd
Fouad Elamoury <foad.osama@hotmail.com>
2025-08-02 18:04:05 +0300

    tagging droplet

--- Assets/ScriptsTesting/UnSortedObjects/UnSortedObject.cs
+++ Assets/ScriptsTesting/UnSortedObjects/UnSortedObject.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class UnSortedObject : MonoBehaviour, ISortable
 {
-   
+    [SerializeField] UnityEvent onFixed;
 
     public void Sort()
     {
 
         if(gameObject.CompareTag("droplet"))
-            { UnityEngine.Object.Destroy(gameObject); }
+        {
+            onFixed.Invoke();
+        }
         Debug.Log($"{gameObject.name} has been sorted.");
         // Add sorting logic here
     }
