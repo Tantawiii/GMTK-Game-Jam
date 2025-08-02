5ab1df5ce055abb77c68d83087e1a5941e01d2e2
Fouad Elamoury <foad.osama@hotmail.com>
2025-08-02 20:42:05 +0300

    organized sorted objects and fix paint frames

--- Assets/ScriptsTesting/UnSortedObjects/UnSortedObject.cs
+++ Assets/ScriptsTesting/UnSortedObjects/UnSortedObject.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 [RequireComponent(typeof(Outline))]
 public class UnSortedObject : MonoBehaviour, ISortable
 {
+    private Animator animator;
     private void Start()
     {
+        animator =  GetComponent<Animator>();
         GetComponent<Outline>().enabled = false;
     }
     public void Sort()
     {
         Debug.Log($"{gameObject.name} has been sorted.");
+        animator.SetBool("IsSorted", true);
         // Add sorting logic here
     }
 }
